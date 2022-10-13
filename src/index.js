b2af57174aaf955f9ae58522bddb3e47f47fe13d
BroKoda <kottix96@gmail.com>
2022-10-13 13:20:40 +0200

    Addign REACT REDUX to the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {configureStore} from "./store";
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
 import App from './App';
 
+const store = configureStore()
+const persistor = persistStore(store)
+
 ReactDOM.render(
-	<App/>,
+	<Provider store={store}>
+		<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+			<App/>
+		</PersistGate>
+	</Provider>,
 	document.getElementById('root')
 )
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {connect} from "react-redux";
 import './TodoList.css';
 import TodoListItem from "./TodoListItem";
 import NewTodoForm from "./NewTodoForm";
+import {removeTodo} from "./actions";
+import {markTodoAsCompleted} from "./actions";
 
-const TodoList = ({todos = [{text: 'Hello'}]}) => (
+const TodoList = ({todos = [], onRemovePressed, onCompletedPressed}) => (
 	<>
 		<div className={'list-wrapper'}>
 			<NewTodoForm/>
-			{todos.map(todo => <TodoListItem todo={todo} />)}
+			{todos.map(todo =>
+				<TodoListItem todo={todo} onRemovePressed={onRemovePressed}
+				onCompletedPressed={onCompletedPressed}/>
+			)}
 		</div>
 	</>
 )
 
-export default TodoList
+const mapStateToProps = state => ({
+	todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+	onRemovePressed: text => dispatch(removeTodo(text)),
+	onCompletedPressed: text => dispatch(markTodoAsCompleted(text))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
